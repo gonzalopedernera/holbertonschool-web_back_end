7957670f7e036d17a726ead0ea66b21f0201b995
gonzalopedernera <gonzalopeder2@gmail.com>
2023-08-28 14:23:30 -0700

    Task 1 compelted, working and Pycodestyle corrected

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function concat
+Takes a string str1 and a string str2 as arguments
+Returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """returns str1 + str2"""
+    return str1 + str2
