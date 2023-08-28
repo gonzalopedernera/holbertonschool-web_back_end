1c3528e36e343b54aed117b27f78a9eef9d1951a
gonzalopedernera <gonzalopeder2@gmail.com>
2023-08-28 14:44:40 -0700

    Task 3 completed, working and Pycodestyle corrected

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""type-annotated function 'to_str'
+Takes a float 'n' as argument and returns the string representation of 'n'.
+"""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of 'n'"""
+    return str(n)
