d0444f86b53b6a16f5c2d99a8e49b1ff4c7e1b1e
gonzalopedernera <gonzalopeder2@gmail.com>
2023-08-28 14:39:04 -0700

    Task 2 completed, working and Pycodestyle corrected

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function 'floor'
+Takes a float 'n' as argument and returns the floor of the float
+"""
+
+
+def floor(n: float) -> int:
+    """Returns the floor of float 'n'"""
+    return int(n)
