341860e3567214fccd546b8474a5a9c539ce7daa
gonzalopedernera <gonzalopeder2@gmail.com>
2023-08-28 14:16:48 -0700

    Task 0 completed, working and Pycodestyle corrected

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Type-annotated function 'add'
+Takes a float a and a float b as arguments and returns their sum as a float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """return the sum of a and b (float)"""
+    return a + b
