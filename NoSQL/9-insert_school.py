44ff994359f66cd5766335b7b3ac76fb97b8495f
gonzalopedernera <gonzalopeder2@gmail.com>
2023-09-04 17:06:48 -0700

    Task 9 completed, working and Pycodestyle corrected

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Inserts a new document in a collection based on kwargs"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Returns the new document '_id'"""
+    return mongo_collection.insert_one(kwargs).inserted_id
