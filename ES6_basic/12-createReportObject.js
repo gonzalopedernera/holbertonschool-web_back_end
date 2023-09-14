d94e8dda259732aab44a4fd0a9e44ceff6ff2685
gonzalopedernera <gonzalopeder2@gmail.com>
2023-09-13 19:29:10 -0700

    Task 12 correction

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,6 +1,5 @@
 export default function createReportObject(employeesList) {
   const obj = {};
-  const allEmployees = {};
-  obj[allEmployees] = employeesList;
+  obj["allEmployees"] = employeesList;
   return obj;
 }
