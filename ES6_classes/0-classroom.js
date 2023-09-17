6701fd3dbd86313ccafa3b5afca499fcc8043998
gonzalopedernera <gonzalopeder2@gmail.com>
2023-09-17 13:56:49 -0700

    Task 0 correction

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,6 +1,6 @@
 // Implement a class named ClassRoom
 export default class ClassRoom {
-  constructor (maxStudentSize) {
+  constructor(maxStudentSize) {
     this._maxStudentSize = maxStudentSize;
   }
 }
