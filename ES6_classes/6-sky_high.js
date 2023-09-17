e20b89963f28e93a43c960fcc3b9efadb9fa9591
gonzalopedernera <gonzalopeder2@gmail.com>
2023-09-17 16:24:30 -0700

    Task 6 completed, working and Eslint friendly

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,28 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  set sqft(sqft) {
+    this._sqft = sqft; 
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set floors(floors) {
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
