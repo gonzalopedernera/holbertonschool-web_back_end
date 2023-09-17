13ee22a1a57158eff0b6230ec1c1c95e6c55569c
gonzalopedernera <gonzalopeder2@gmail.com>
2023-09-17 15:20:27 -0700

    Task 2 eslint

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -2,30 +2,26 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name === 'string') {
       this._name = name;
-    }
-    else {
-      throw new TypeError("Name must be a string");
+    } else {
+      throw new TypeError('Name must be a string');
     }
     if (typeof length === 'number') {
       this._length = length;
-    }
-    else {
-      throw new TypeError("Length must be a number");
+    } else {
+      throw new TypeError('Length must be a number');
     }
     if (Array.isArray(students)) {
       this._students = students;
-    }
-    else {
-      throw new TypeError("Students must be an array");
+    } else {
+      throw new TypeError('Students must be an array');
     }
   }
 
   set name(name) {
     if (typeof name === 'string') {
       this._name = name;
-    }
-    else {
-      throw new TypeError("Name must be a string");
+    } else {
+      throw new TypeError('Name must be a string');
     }
   }
   get name() {
@@ -35,9 +31,8 @@ export default class HolbertonCourse {
   set length(length) {
     if (typeof length === 'number') {
       this._length = length;
-    }
-    else {
-      throw new TypeError("Length must be a number");
+    } else {
+      throw new TypeError('Length must be a number');
     }
   }
   get length() {
@@ -47,9 +42,8 @@ export default class HolbertonCourse {
   set students(students) {
     if (Array.isArray(students)) {
       this._students = students;
-    }
-    else {
-      throw new TypeError("Students must be an array");
+    } else {
+      throw new TypeError('Students must be an array');
     }
   }
   get students() {
