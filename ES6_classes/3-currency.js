bce265cd79fcca22d44e4909f52789bd6c2cd300
gonzalopedernera <gonzalopeder2@gmail.com>
2023-09-17 15:37:57 -0700

    Task 3 completed, working and Eslint friendly

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,42 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof(code) == 'string') {
+      this._code = code;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
+    if (typeof(name) == 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  set code(code) {
+    if (typeof(code) == 'string') {
+      this._code = code;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(name) {
+    if (typeof(name) == 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
