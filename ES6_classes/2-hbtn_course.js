bebda5090676f4974dbfce7d1c2fe7a4d1d93c1c
gonzalopedernera <gonzalopeder2@gmail.com>
2023-09-17 15:11:15 -0700

    Task 2 correction

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -3,30 +3,54 @@ export default class HolbertonCourse {
     if (typeof name === 'string') {
       this._name = name;
     }
+    else {
+      throw new TypeError("Name must be a string");
+    }
     if (typeof length === 'number') {
       this._length = length;
+    }
+    else {
+      throw new TypeError("Length must be a number");
     }
     if (Array.isArray(students)) {
       this._students = students;
     }
+    else {
+      throw new TypeError("Students must be an array");
+    }
   }
 
   set name(name) {
-    this._name = name;
+    if (typeof name === 'string') {
+      this._name = name;
+    }
+    else {
+      throw new TypeError("Name must be a string");
+    }
   }
   get name() {
     return this._name;
   }
 
   set length(length) {
-    this._length = length;
+    if (typeof length === 'number') {
+      this._length = length;
+    }
+    else {
+      throw new TypeError("Length must be a number");
+    }
   }
   get length() {
     return this._length;
   }
 
   set students(students) {
-    this._students = students;
+    if (Array.isArray(students)) {
+      this._students = students;
+    }
+    else {
+      throw new TypeError("Students must be an array");
+    }
   }
   get students() {
     return this._students;
